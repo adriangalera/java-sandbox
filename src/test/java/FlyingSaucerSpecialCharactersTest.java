e62dabf66cfa139bf760d05cbbc068950918e5ff
Adrian Galera <adrian.galera@ocado.com>
2020-07-27 16:44:57 +0200

    Trying to render polish characters

--- src/test/java/FlyingSaucerSpecialCharactersTest.java
+++ src/test/java/FlyingSaucerSpecialCharactersTest.java
@@ -7,10 +7,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Date;
+import org.junit.Assert;
 import org.junit.Test;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-public class FlyingSaucerPdfJapaneseTest {
+public class FlyingSaucerSpecialCharactersTest {
 
     @Test
     public void templateShouldRenderJapaneseCharacters() throws DocumentException, IOException, URISyntaxException {
@@ -29,9 +30,31 @@ public class FlyingSaucerPdfJapaneseTest {
             + "</body>\n"
             + "</html>";
         byte[] pdfContents = convertToPdf(html);
-        writePdf(pdfContents);
+        String filename = "japanese-" + new Date().toString() + ".pdf";
+        writePdf(pdfContents, filename);
+        Assert.fail("to do text extraction on generated pdf");
+    }
 
-
+    @Test
+    public void templateShouldRenderPolishCharacters() throws DocumentException, IOException, URISyntaxException {
+        String html = "<html>\n"
+            + "<head>\n"
+            + "    <style>\n"
+            + "        body{\n"
+            + "            font: 15px 'Abhaya Libre', sans-serif;\n"
+            + "        }\n"
+            + "    </style>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "<p>Following text will contain polish characters</p>\n"
+            + "<p>Zażółć gęślą jaźń</p>\n"
+            + "<p>End of polish characters</p>\n"
+            + "</body>\n"
+            + "</html>";
+        byte[] pdfContents = convertToPdf(html);
+        String filename = "polish-" + new Date().toString() + ".pdf";
+        writePdf(pdfContents, filename);
+        Assert.fail("to do text extraction on generated pdf");
     }
 
     private byte[] convertToPdf(String html) throws DocumentException, IOException, URISyntaxException {
@@ -61,7 +84,7 @@ public class FlyingSaucerPdfJapaneseTest {
     }
 
     private File getFontsDirectory() throws URISyntaxException {
-        return new File(FlyingSaucerPdfJapaneseTest.class.getResource("/fonts").toURI());
+        return new File(FlyingSaucerSpecialCharactersTest.class.getResource("/fonts").toURI());
     }
 
     private static class Constants {
@@ -69,8 +92,7 @@ public class FlyingSaucerPdfJapaneseTest {
         private static final boolean FINISH_PDF = true;
     }
 
-    private void writePdf(byte[] pdfContents) {
-        final String pdfFileName = "generated" + new Date().toString() + ".pdf";
+    private void writePdf(byte[] pdfContents, String pdfFileName) {
         try (FileOutputStream fos = new FileOutputStream(pdfFileName)) {
             fos.write(pdfContents);
         } catch (IOException e) {
